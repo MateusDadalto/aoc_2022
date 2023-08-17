770be11636fa3767abbb0cd302b96b2b5aa82269
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-08-17 07:46:47 +0000

    day 4 part 2

--- src/day_four.rs
+++ src/day_four.rs
@@ -1,3 +1,5 @@
+use std::ops::RangeInclusive;
+
 use super::helper;
 
 struct Assignment {
@@ -15,10 +17,30 @@ impl Assignment {
         }
     }
 
-    fn contains(&self, other: &Assignment) -> bool {
-        self.start <= other.start && self.end >= other.end
+    fn range(&self) -> RangeInclusive<u8> {
+        self.start..=self.end
     }
 
+    fn overlaps(&self, other: &Assignment) -> bool {
+        self.contains_start(&other) || self.contains_end(&other)
+            || other.contains_start(self) || other.contains_end(self)
+    }
+
+    fn contains_start(&self, other: &Assignment) -> bool {
+        
+        self.range().contains(&other.start)
+    }
+    
+    fn contains_end(&self, other: &Assignment) -> bool {
+        self.range().contains(&other.end)
+    }
+
+    #[allow(dead_code)]
+    fn contains(&self, other: &Assignment) -> bool {
+        self.contains_start(&other) && self.contains_end(&other)
+    }
+
+    #[allow(dead_code)]
     fn is_contained(&self, other: &Assignment) -> bool {
         other.contains(self)
     }
@@ -32,10 +54,48 @@ pub fn solve() {
         let mut pair: Vec<Assignment> = line.split(',').map(|range| Assignment::new(range)).collect();
         let pair: (Assignment, Assignment) = (pair.pop().unwrap(), pair.pop().unwrap());
 
-        if pair.0.contains(&pair.1) || pair.0.is_contained(&pair.1) {
+        if pair.0.overlaps(&pair.1) {
             counter += 1;
         }
     }
 
     println!("DAY 4 PART 1 ANSWER: {counter}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn assignment_overlaps() {
+        let a1 = Assignment { start: 6, end: 10 };
+        let a2 = Assignment { start: 8, end: 12 };
+        let a3 = Assignment { start: 2, end: 7 };
+        let a4 = Assignment { start: 9, end: 9 };
+        let a5 = Assignment { start: 1, end: 9 };
+
+        // overlaps
+        assert!(a1.overlaps(&a2));
+        assert!(a1.overlaps(&a3));
+        assert!(a1.overlaps(&a4));
+        assert!(a1.overlaps(&a5));
+        assert!(a2.overlaps(&a4));
+        assert!(a5.overlaps(&a4), "Should be inclusive range");
+        
+        // opposite should also overlap
+        assert!(a2.overlaps(&a1));
+        assert!(a3.overlaps(&a1));
+        assert!(a4.overlaps(&a1));
+        assert!(a5.overlaps(&a1));
+        assert!(a4.overlaps(&a2));
+        assert!(a4.overlaps(&a5));
+
+        //should not overlap
+        assert!(!a4.overlaps(&a3));
+        assert!(!a2.overlaps(&a3));
+
+        // opposite also should not overlap
+        assert!(!a3.overlaps(&a4));
+        assert!(!a3.overlaps(&a2));
+    }
 }
