03556170793697be3a6e5ce03fdedecdf4d52cee
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-09-11 20:48:21 +0000

    Day 20 part 2
    Still mad with you euclid

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,31 @@ const EXAMPLE: &str = include_str!("../inputs/example.txt");
 const INPUT: &str = include_str!("../inputs/input.txt");
 
 pub fn solve() {
-    // let input: Vec<i32> = EXAMPLE.lines().map(|l| l.parse::<i32>().unwrap()).collect();
-    let input: Vec<i32> = INPUT.lines().map(|l| l.parse::<i32>().unwrap()).collect();
+    let decryption_key = 811589153;
+
+    // let input: Vec<i64> = EXAMPLE.lines().map(|l| l.parse::<i64>().unwrap() * decryption_key).collect();
+    let input: Vec<i64> = INPUT.lines().map(|l| l.parse::<i64>().unwrap() * decryption_key).collect();
     let mut shifter: Vec<_> = (0..input.len()).collect();
     let mut zero_idx = 0;
-    for (i, &n) in input.iter().enumerate() {
-        if n == 0 {
-            zero_idx = i;
-            continue;
+
+    for _ in 0..10  {
+        for (i, &n) in input.iter().enumerate() {
+            if n == 0 {
+                zero_idx = i;
+                continue;
+            }
+    
+            let mixed_idx = shifter.iter().position(|m_i| *m_i == i).unwrap();
+    
+            shifter.remove(mixed_idx);
+            // living and learning, my overcomplicated solution can be instead this simple F_ing method
+            // damn you modulus and remainder
+            let new_mixed_idx = (mixed_idx as i64 + n).rem_euclid(shifter.len() as i64) as usize;
+    
+            shifter.insert(new_mixed_idx, i);
         }
-
-        let mixed_idx = shifter.iter().position(|m_i| *m_i == i).unwrap();
-
-        shifter.remove(mixed_idx);
-        // living and learning, my overcomplicated solution can be instead this simple F_ing method
-        // damn you modulus and remainder
-        let new_mixed_idx = (mixed_idx as i32 + n).rem_euclid(shifter.len() as i32) as usize;
-
-        shifter.insert(new_mixed_idx, i);
     }
-
+    
     let zero_idx = shifter.iter().position(|s_i| *s_i == zero_idx).unwrap();
     println!("0 index {zero_idx}");
 
