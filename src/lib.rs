e467546d23c8572af04d295c09236f6ec82e7816
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2023-09-12 02:16:41 +0000

    Day 21 part 1
    Finally another one I can do by myself

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,85 @@
+use std::{hash::{Hash, Hasher}, collections::HashMap, ops::{Add, Sub, Mul, Div}};
+
 const EXAMPLE: &str = include_str!("../inputs/example.txt");
 const INPUT: &str = include_str!("../inputs/input.txt");
 
 pub fn solve() {
-    EXAMPLE.lines();
-    INPUT.lines();
+    // let monkeys: HashMap<String, Monkey> = EXAMPLE.lines().map(Monkey::parse).map(|m| (m.name.clone(), m)).collect();
+    let monkeys: HashMap<String, Monkey> = INPUT.lines().map(Monkey::parse).map(|m| (m.name.clone(), m)).collect();
+
+    let r = monkeys.get("root").unwrap().eval(&monkeys);
+
+    println!("{r:?}");
+}
+
+#[derive(Debug, Clone)]
+struct Monkey {
+    name: String,
+    kind: MonkeyType,
+    value: String,
+}
+
+impl Monkey {
+    fn parse(line: &str) -> Self {
+        let name = line[0..4].to_string();
+
+        let value = line[6..].to_string();
+
+        let kind = match value.parse::<i64>() {
+            Ok(_) => MonkeyType::Value,
+            Err(_) => MonkeyType::Operation,
+        };
+
+        Monkey { name, kind, value }
+    }
+
+    fn eval(&self, monkeys: &HashMap<String, Monkey>) -> i64 {
+        match self.kind {
+            MonkeyType::Value => self.value.parse().unwrap(),
+            MonkeyType::Operation => {
+                let content: Vec<_> = self.value.split_whitespace().collect();
+                let monkey1 = monkeys.get(content[0]).unwrap().eval(monkeys);
+                let operator = Operator::parse(content[1]);
+                let monkey2 = monkeys.get(content[2]).unwrap().eval(monkeys);
+
+                match operator {
+                    Operator::Add => monkey1.add(monkey2),
+                    Operator::Sub => monkey1.sub(monkey2),
+                    Operator::Multi => monkey1.mul(monkey2),
+                    Operator::Div => monkey1.div(monkey2),
+                }
+            },
+        }
+    }
+}
+
+impl Hash for Monkey {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum MonkeyType {
+    Operation,
+    Value,
+}
+
+enum Operator {
+    Add,
+    Sub,
+    Multi,
+    Div,
+}
+
+impl Operator {
+    fn parse(s: &str) -> Self {
+        match s {
+            "+" => Self::Add,
+            "-" => Self::Sub,
+            "*" => Self::Multi,
+            "/" => Self::Div,
+            any => panic!("Invalid string for operator: {any}")
+        }
+    }
 }
